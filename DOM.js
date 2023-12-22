8f2a950c63309016e6379091d5ae251ac38491ee
alumno <alumno@Instituto.local>
2023-12-22 08:59:19 -0300

    holaa

---
+++ DOM.js
@@ -0,0 +1,15 @@
+
+let botonRojo = document.querySelector('#negro'); 
+botonRojo.onclic = function cambiar(){
+img.src = 'imagenes/cronosRojo'
+let botonBlanco = document.querySelector('.imagenes')
+botonBlanco = 'imagenes/cronosRojo'
+}
+
+letbotonBlanco = document.querySelector('#negro')
+botonBlanco.onclick = function cambiar(){
+
+    let botonBlanco = document.querySelector(imagenes)
+    botonBlanco = 'imagenes/cronosBlanco'
+    img.src = 'imagenes/cronpsBlanco'
+}
